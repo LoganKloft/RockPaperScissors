8de5bc673164e768711b4bd978e71a13f94cb225
Logan Kloft <lkloft@outlook.com>
2022-07-26 15:31:37 -0700

    Implement logic for RPS

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -11,83 +11,100 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection == "Rock") {
         switch (computerSelection) {
             case "Rock":
-                console.log(`Tie! ${playerSelection} ties ${computerSelection}`);
+                results.innerText = `Tie! ${playerSelection} ties ${computerSelection}`;
                 return 0;
             case "Scissors":
-                console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+                results.innerText = `You win! ${playerSelection} beats ${computerSelection}`;
                 return 1;
             case "Paper":
-                console.log(`You lose! ${playerSelection} loses to ${computerSelection}`);
+                results.innerText = `You lose! ${playerSelection} loses to ${computerSelection}`;
                 return -1;
             default:
-                console.log(`Not a valid Player Selection: ${playerSelection}`);
+                results.innerText = `Not a valid Player Selection: ${playerSelection}`;
                 return 0;
         }
     } else if (playerSelection == "Paper") {
         switch (computerSelection) {
             case "Paper":
-                console.log(`Tie! ${playerSelection} ties ${computerSelection}`);
+                results.innerText = `Tie! ${playerSelection} ties ${computerSelection}`;
                 return 0;
             case "Rock":
-                console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+                results.innerText = `You win! ${playerSelection} beats ${computerSelection}`;
                 return 1;
             case "Scissors":
-                console.log(`You lose! ${playerSelection} loses to ${computerSelection}`);
+                results.innerText = `You lose! ${playerSelection} loses to ${computerSelection}`;
                 return -1;
             default:
-                console.log(`Not a valid Player Selection: ${playerSelection}`);
+                results.innerText = `Not a valid Player Selection: ${playerSelection}`;
                 return 0;
         }
     } else if (playerSelection == "Scissors") {
         switch (computerSelection) {
             case "Scissors":
-                console.log(`Tie! ${playerSelection} ties ${computerSelection}`);
+                results.innerText = `Tie! ${playerSelection} ties ${computerSelection}`;
                 return 0;
             case "Paper":
-                console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+                results.innerText = `You win! ${playerSelection} beats ${computerSelection}`;
                 return 1;
             case "Rock":
-                console.log(`You lose! ${playerSelection} loses to ${computerSelection}`);
+                results.innerText = `You lose! ${playerSelection} loses to ${computerSelection}`;
                 return -1;
             default:
-                console.log(`Not a valid Player Selection: ${playerSelection}`);
+                results.innerText = `Not a valid Player Selection: ${playerSelection}`;
                 return 0;
         }
     } else {
-        console.log(`Not a valid Player Selection: ${playerSelection}`);
+        results.innerText = `Not a valid Player Selection: ${playerSelection}`;
         return 0;
     }
 }
 
-function game() {
-    let playerSelection = null;
-    let computerSelection = null;
-    let playerScore = 0;
-    let computerScore = 0;
+function disableButtons() {
+    rockButton.removeEventListener('click', chooseRock);
+    scissorsButton.removeEventListener('click', chooseScissors);
+    paperButton.removeEventListener('click', choosePaper);
+}
 
-    for(let i = 0; i < 5; i++) {
-        computerSelection = getComputerChoice();
-        playerSelection = prompt("Rock, Paper, or Scissors?");
-
-        const result = playRound(playerSelection, computerSelection);
-
-        if (result == 1) {
-            playerScore++;
-        } else if (result == -1){
-            computerScore++;
-        }
-
-        console.log(`Running score: (${playerScore}, ${computerScore})`);
+function choose(playerSelection) {
+    let result = playRound(playerSelection, getComputerChoice());
+    if (result > 0) {
+        playerScore++;
+    } else if (result < 0) {
+        computerScore++;
     }
 
-    console.log(`Final score: (${playerScore}, ${computerScore})`);
-    if (playerScore > computerScore) {
-        console.log("You win!");
-    } else if (playerScore < computerScore) {
-        console.log("You lose!");
+    if (playerScore === 5) {
+        score.innerText = `Game over. Player wins! Final Score: ${playerScore} ${computerScore}`;
+        disableButtons();
+    } else if (computerScore === 5) {
+        score.innerText = `Game over. Computer wins! Final Score: ${playerScore} ${computerScore}`;
+        disableButtons();
     } else {
-        console.log("Tie!");
+        score.innerText = `${playerScore} ${computerScore}`;
     }
 }
 
-game();
+function chooseRock() {
+    choose("Rock");
+}
+
+function chooseScissors() {
+    choose("Scissors");
+}
+
+function choosePaper() {
+    choose("Paper");
+}
+
+let results = document.querySelector('#results');
+let score = document.querySelector('#score');
+let playerScore = 0;
+let computerScore = 0;
+
+let rockButton = document.querySelector('#rock');
+let scissorsButton = document.querySelector('#scissors');
+let paperButton = document.querySelector('#paper');
+
+rockButton.addEventListener('click', chooseRock);
+scissorsButton.addEventListener('click', chooseScissors);
+paperButton.addEventListener('click', choosePaper);
